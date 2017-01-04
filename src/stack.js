8fe52fe7aa614f95549f7db10bd91a2cdb03df2c
jason00111 <emberleyjason@gmail.com>
2017-01-03 20:14:58 -0800

    Impliment stack

--- src/stack.js
+++ src/stack.js
@@ -1,5 +1,39 @@
 'use strict'
 
 export default class Stack {
-  // your code here
+  constructor() {
+    this.top = 0
+    this.data = []
+  }
+
+  push(element) {
+    this.top++
+    this.data[this.top] = element
+  }
+
+  pop() {
+    if (this.top === 0) {
+      return null
+    } else {
+      this.top--
+      return this.data[this.top + 1]
+    }
+  }
+
+  peek() {
+    if (this.top === 0) {
+      return null
+    } else {
+      return this.data[this.top]
+    }
+  }
+
+  isEmpty() {
+    if (this.top === 0) return true
+    else return false
+  }
+
+  length() {
+    return this.top
+  }
 }
